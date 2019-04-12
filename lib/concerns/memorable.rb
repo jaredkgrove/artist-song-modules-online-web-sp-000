8194d3727b924e4d31d2ede806b915dfb6a4aeec
Jared Grove <jaredkgrove@gmail.com>
2019-04-12 17:34:20 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,13 +1,18 @@
 
 module Memorable
-  def initialize
-    
-  end
-  def reset_all
-    all.clear
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
+
+    def count
+      all.count
+    end
   end
 
-  def count
-    all.count
+  module InstanceMethods
+    def initialize
+    
+    end
   end
 end
