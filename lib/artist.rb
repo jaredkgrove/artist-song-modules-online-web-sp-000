966886006fc6584d209c21487b87abcd3c03f29b
Jared Grove <jaredkgrove@gmail.com>
2019-04-12 17:39:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,24 +15,10 @@ class Artist
     @songs = []
   end
 
-  
-  #def self.find_by_name(name)
-  #  @@artists.detect{|a| a.name == name}
-  #end
-
   def self.all
     @@artists
   end
 
-=begin
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-=end
   def add_song(song)
     @songs << song
     song.artist = self
@@ -41,8 +27,4 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  #def to_param
-  #  name.downcase.gsub(' ', '-')
-  #end
 end
